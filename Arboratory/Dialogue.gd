9121ed7946395d5a3616541f1692efbac887038d
LowQualityCod-e <70413928+LowQualityCod-e@users.noreply.github.com>
2020-11-12 16:21:00 -0500

    Revert "Sound Test for Compatability Merge"
    This reverts commit d7fe07236e5023fc87d68e5d930f4ae0aadf1542.

--- Arboratory/Dialogue.gd
+++ Arboratory/Dialogue.gd
@@ -19,7 +19,6 @@ func _ready():
 	set_process_input(true)
 	
 func select_dialogue():
-	get_tree().get_root().find_node("Dialogue Audio", true, false).play()
 	match Leveling.level:
 		1:
 			print('successfully did the thing')
